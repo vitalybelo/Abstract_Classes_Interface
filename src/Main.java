6bbccbea070d5d65d7c6dcc274e0dd8dde541d8b
Vitaly <39009460+vitalybelo@users.noreply.github.com>
2022-12-02 10:43:39 +0300

    checkup

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,14 @@ public class Main
         lamps.add(new Lamp(50));
         lamps.add(new Lamp(150));
 
-        Collections.sort(lamps);
-        //lamps.get(0).setType(Lamp.Type.FILAMENT);
-
         for (Lamp lamp : lamps )
             System.out.println(lamp);
+
+        Collections.sort(lamps);
+        System.out.println();
+        for (Lamp lamp : lamps )
+            System.out.println(lamp);
+
+
     }
 }
